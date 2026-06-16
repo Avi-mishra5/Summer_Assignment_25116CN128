f5d0715033848499f2abd08e0da59783714588a7
Avi-mishra5 <mishraavi522006@gmail.com>
2026-06-16 22:33:30 +0530

    add day 16 prgs

---
+++ DAY 16/program62.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the terms: ");
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    int maxFreq=0,maxEl;
+    for(int i=0;i<n;i++)
+    {
+        int count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==arr[i])
+            count++;
+        }
+        if(count>maxFreq)
+        {
+            maxFreq=count;
+            maxEl=arr[i];
+        }
+    }
+    printf("Maximum Frequency element is %d",maxEl);
+    return 0;
+}
---
+++ DAY 16/program63.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+    int n, sum, found = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter array elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter required sum: ");
+    scanf("%d", &sum);
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == sum) {
+                printf("Pair found: %d and %d\n", arr[i], arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if(found)
+            break;
+    }
+    if(!found)
+        printf("No pair found.");
+    return 0;
+}
