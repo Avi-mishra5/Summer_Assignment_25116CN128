89a3b59c625bd27efb29ac2e64d5369880e8610d
Avi-mishra5 <mishraavi522006@gmail.com>
2026-06-20 22:37:00 +0530

    add day 20 prgs

---
+++ DAY 20/program78.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j, flag = 1;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    if (r != c) {
+        printf("Matrix is not symmetric.");
+        return 0;
+    }
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i < r; i++)
+        for (j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            if (a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 0)
+            break;
+    }
+
+    if (flag == 1)
+        printf("Matrix is symmetric.");
+    else
+        printf("Matrix is not symmetric.");
+
+    return 0;
+}
